3c4f27fc00fda44fe653bff0bfd00bdb4e10cf64
Courbayre Basil <basil.courbayre@ecole.ensicaen.fr>
2023-11-02 15:23:13 -0400

    changed main name

---
+++ src/main_pipeline.py
@@ -0,0 +1,76 @@
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler, OrdinalEncoder
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+# Sample dataset (replace with your dataset)
+#custom_column_names = ["ID", "adultsCount", "ChildrenCount", "WENightsCount", "WeekNightsCount", "MealType", "ParkingSpaceRequired", "RoomType", "LeadTime", "ArrivalYear", "ArrivalMonth", "ArrivalDay", "MarketSegmentType", "RepeatedGuest", "NpPreviousCancellations"]
+df = pd.read_csv('data/Hotel_Reservations.csv')
+print(df.head(1))
+
+# Define features and target
+features = df.columns.difference(['Booking_ID', 'booking_status'])
+
+
+categorical_cols = ['type_of_meal_plan', 'room_type_reserved', 'market_segment_type', "booking_status"]
+
+encoder = OrdinalEncoder()
+df[categorical_cols] = encoder.fit_transform(df[categorical_cols])
+
+scaler = StandardScaler()
+df[features] = scaler.fit_transform(df[features])
+print(df)
+
+X = df[features]
+y = df['booking_status']
+
+# Split the data into train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# Define classifiers
+classifiers = {
+    'KNN': KNeighborsClassifier(),
+    'Decision Tree': DecisionTreeClassifier(),
+    'Random Forest': RandomForestClassifier()
+}
+
+# Define hyperparameter grids for grid search
+param_grids = {
+    'KNN': {
+        'n_neighbors': [3, 5, 7],
+        'p': [1, 2]
+    },
+    'Decision Tree': {
+        'max_depth': [None, 5, 10],
+        'min_samples_split': [2, 5]
+    },
+    'Random Forest': {
+        'n_estimators': [10, 50, 100],
+        'max_depth': [None, 5, 10]
+    }
+}
+
+results = {}
+
+# Perform grid search for each classifier and hyperparameters
+for name, classifier in classifiers.items():
+    clf = GridSearchCV(classifier, param_grids[name], scoring='accuracy', cv=3)
+    clf.fit(X_train, y_train)
+    best_estimator = clf.best_estimator_
+    
+    # Evaluate the best estimator on the test set
+    y_pred = best_estimator.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    results[name] = {
+        'best_estimator': best_estimator,
+        'accuracy': accuracy
+    }
+
+# Print the results
+for name, result in results.items():
+    print(f'{name} Accuracy: {result["accuracy"]:.2f}')
+    print(f'Best Estimator for {name}:\n{result["best_estimator"]}\n')
