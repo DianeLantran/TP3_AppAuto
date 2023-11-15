eaddb0706782c30f018c6172f288b3cf414243ff
Mathilde <raby.m@outlook.fr>
2023-11-15 12:30:30 +0100

    Graph scores

--- evaluationUtils.py
+++ evaluationUtils.py
@@ -125,6 +125,7 @@ def scores(y_test, y_pred, name):
     precision = metrics.precision_score(y_test, y_pred)
     recall = metrics.recall_score(y_test, y_pred)
     f1 = metrics.f1_score(y_test, y_pred)
+    accuracy = metrics.accuracy_score(y_test, y_pred)
 
     print(f'Methode: {name}')
     print("Précision:", precision)
@@ -134,3 +135,36 @@ def scores(y_test, y_pred, name):
     report = metrics.classification_report(y_test, y_pred)
     print("Rapport de classification:")
     print(report)
+
+def graphScores(resultsList):
+    model_names = [result[0] for result in resultsList]
+    model_names = [result[0] for result in resultsList]
+    precision_scores = [metrics.precision_score(result[1], result[2]) for result in resultsList]
+    recall_scores = [metrics.recall_score(result[1], result[2]) for result in resultsList]
+    f1_scores = [metrics.f1_score(result[1], result[2]) for result in resultsList]
+    accuracy_scores = [metrics.accuracy_score(result[1], result[2]) for result in resultsList]
+
+    # Largeur des barres
+    bar_width = 0.15
+
+    # Positions des barres pour chaque modèle
+    index = np.arange(len(model_names))
+
+    colors = ['#ff7473','#ffc952', '#47b8e0', '#34314c']
+
+    # Tracer le graphique en barres groupées
+    plt.figure(figsize=(12, 6))
+
+    plt.bar(index, precision_scores, width=bar_width, label='Precision', color=colors[0])
+    plt.bar(index + bar_width, recall_scores, width=bar_width, label='Recall', color=colors[1])
+    plt.bar(index + 2 * bar_width, f1_scores, width=bar_width, label='F1-score', color=colors[2])
+    plt.bar(index + 3 * bar_width, accuracy_scores, width=bar_width, label='Accuracy', color=colors[3])
+
+    # Ajouter des détails au graphique
+    plt.xlabel('Model', fontweight='bold')
+    plt.ylabel('Scores', fontweight='bold')
+    plt.title('Performances comparison for all models')
+    plt.xticks(index + 1.5 * bar_width, model_names)
+    plt.legend()
+
+    plt.show()
